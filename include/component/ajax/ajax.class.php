74653de6416f3214a132ad13e33ae40e0e3fe935
puja161 <puja.kumari606@gmail.com>
2014-10-09 14:56:51 +0530

    added
    code

---
+++ include/component/ajax/ajax.class.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * [PHPFOX_HEADER]
+ */
+
+defined('PHPFOX') or exit('NO DICE!');
+
+/**
+ * AJAX class for the thought module. Used to add and get shoutouts.
+ * 
+ * @copyright		[PHPFOX_COPYRIGHT]
+ * @author  		Raymond Benc
+ * @package  		Module_thought
+ * @version 		$Id: ajax.class.php 6909 2013-11-20 14:01:14Z Miguel_Espinoza $
+ */
+class Thought_Component_Ajax_Ajax extends Phpfox_Ajax
+{
+	/**
+	 * Add a new shoutout
+	 *
+	 * @return boolean Return false if we ran into an error.
+	 */
+	
+	
+	
+	/**
+	 * Get the latest thought messages.
+	 *
+	 */
+	
+	}
+
+?>
